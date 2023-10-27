74c77a87a3ce8b057f7881ee0c942c8776262816
Max <max_morin99@hotmail.com>
2023-10-26 22:13:02 -0400

    remise.

---
+++ src/entrypoint.c
@@ -0,0 +1,425 @@
+#include "entrypoint.h"
+
+#include "game.h"
+
+#include <raylib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
+
+/**
+ * @brief Q-learning algorithm settings.
+ *
+ */
+#define QLEARN_N_STATES 16 // 4 bits i.e. 2^4, can be up to 8 bits i.e. 2^8
+#define QLEARN_N_ACTIONS 5
+#define QLEARN_LEARNING_RATE 0.1f
+#define QLEARN_DISCOUNT_FACTOR 0.9f
+#define QLEARN_EPSILON_RATE 0.9999f
+
+
+/* Public typedefs ---------------------------------------------------------- */
+
+/**
+ * @brief Q-value data type.
+ *
+ */
+typedef float qvalue_t;
+
+/**
+ * @brief Reward data type.
+ *
+ */
+typedef int16_t reward_t;
+
+/**
+ * @brief State data type.
+ *
+ */
+typedef uint16_t state_t;
+
+/**
+ * @brief Action data type.
+ *
+ */
+typedef uint8_t action_t;
+
+/**
+ * @brief Q-learning opaque data type.
+ *
+ */
+typedef struct qlearn qlearn_t;
+
+/**
+ * @brief Q-table configuration data structure.
+ *
+ */
+typedef struct qtable_conf
+{
+    const uint32_t n_states;
+    const uint8_t n_actions;
+    const float learning_rate;
+    const float discount_factor;
+    const float epsilon_rate;
+} qtable_conf_t;
+
+/**
+ * @brief Q-learning configuration data structure.
+ *
+ */
+typedef struct qlearn_conf
+{
+    void *game;
+    void (*restart)(void);
+    bool (*is_ended)(void );
+    void (*apply_action)( uint8_t);
+    state_t (*get_state)(void);
+    reward_t (*get_reward)(void);
+    qtable_conf_t *params;
+} qlearn_conf_t;
+
+/* Private typedefs --------------------------------------------------------- */
+
+/**
+ * @brief Q-learning algorithm data structure.
+ *
+ */
+typedef struct qlearn
+{
+    float **table;
+    void (*restart)();
+    bool (*is_ended)();
+    void (*apply_action)( action_t);
+    state_t (*get_state)();
+    reward_t (*get_reward)();
+    uint32_t n_states;
+    uint8_t n_actions;
+    float learning_rate;
+    float discount_factor;
+    float epsilon;
+    float epsilon_rate;
+} qlearn_t;
+
+/* Public functions --------------------------------------------------------- */
+
+/**
+ * @brief Save Q-table to file in CSV format.
+ *
+ * @param qlearn Q-learning instance.
+ * @param filename File path where to save Q-table values.
+ */
+void qlearn_save_table(qlearn_t *qlearn, const char *filename)
+{
+    FILE *fptr = fopen(filename, "w");
+
+    for (uint32_t i = 0; i < qlearn->n_states; i++)
+    {
+        for (uint8_t j = 0; j < qlearn->n_actions; j++)
+        {
+            fprintf(fptr, "%f,", qlearn->table[i][j]);
+        }
+        fprintf(fptr, "\n");
+    }
+
+    fclose(fptr);
+}
+
+/**
+ * @brief Load Q-table values inside Q-learning instance.
+ *
+ * @param qlearn Q-learning instance.
+ * @param filename File path where to read Q-table values.
+ */
+void qlearn_load_table(qlearn_t *qlearn, const char *filename)
+{
+    FILE *fptr = fopen(filename, "r");
+
+    char line[100];
+    for (uint32_t i = 0; i < qlearn->n_states; i++)
+    {
+        fgets(line, 100, fptr);
+        char *token = strtok(line, ",");
+        for (uint8_t j = 0; j < qlearn->n_actions; j++)
+        {
+            qlearn->table[i][j] = (float)atof(token);
+            token = strtok(NULL, ",");
+        }
+    }
+
+    qlearn->epsilon = 0.0;
+
+    fclose(fptr);
+}
+
+/**
+ * @brief Initialize Q-learning algorithm.
+ *
+ * @param conf Q-learning configurations.
+ * @return qlearn_t*
+ */
+qlearn_t *qlearn_init(qlearn_conf_t *conf)
+{
+    qvalue_t **table = calloc(conf->params->n_states, sizeof(qvalue_t *));
+    for (uint32_t i = 0; i < conf->params->n_states; i++)
+    {
+        table[i] = calloc(conf->params->n_actions, sizeof(qvalue_t));
+    }
+
+    qlearn_t *qlearn = malloc(sizeof(qlearn_t));
+    // Game APIs
+    qlearn->restart = conf->restart;
+    qlearn->is_ended = conf->is_ended;
+    qlearn->apply_action = conf->apply_action;
+    qlearn->get_state = conf->get_state;
+    qlearn->get_reward = conf->get_reward;
+    // Q-table
+    qlearn->table = table;
+    // Q-learning parameters
+    qlearn->n_states = conf->params->n_states;
+    qlearn->n_actions = conf->params->n_actions;
+    qlearn->learning_rate = conf->params->learning_rate;
+    qlearn->discount_factor = conf->params->discount_factor;
+    qlearn->epsilon = 1.0f;
+    qlearn->epsilon_rate = conf->params->epsilon_rate;
+
+    return qlearn;
+}
+
+/**
+ * @brief Deinitialize Q-learning algorithm.
+ *
+ * @param qlearn Q-learning instance.
+ */
+void qlearn_deinit(qlearn_t *qlearn)
+{
+    for (uint16_t i = 0; i < qlearn->n_states; i++)
+    {
+        free(qlearn->table[i]);
+    }
+    free(qlearn->table);
+    free(qlearn);
+}
+
+/**
+ * @brief Restart training episode.
+ *
+ * @param qlearn Q-learning instance.
+ */
+void qlearn_restart(qlearn_t *qlearn)
+{
+    qlearn->restart();
+}
+
+/**
+ * @brief Check if game has ended.
+ *
+ * @param qlearn Q-learning instance.
+ * @return true
+ * @return false
+ */
+bool qlearn_is_ended(qlearn_t *qlearn)
+{
+    qlearn->epsilon *= qlearn->epsilon_rate;
+    return qlearn->is_ended();
+}
+/**
+ * @brief Get reward for taking action a on state S.
+ *
+ * @param qlearn Q-learning instance.
+ * @return reward_t
+ */
+reward_t qlearn_get_reward(qlearn_t *qlearn)
+{
+    return qlearn->get_reward();
+}
+
+
+/**
+ * @brief Get state representation S.
+ *
+ * @param qlearn Q-learning instance.
+ * @return state_t
+ */
+state_t qlearn_get_state(qlearn_t *qlearn)
+{
+    state_t state = qlearn->get_state();
+
+    // Debug log message
+    // printf("Epsilon: %f\tReward: %d\tState: %c %c %c %c \n",
+    //        qlearn->epsilon,
+    //        qlearn_get_reward(qlearn),
+    //        (state & 0x1) ? '1' : '0',
+    //        (state & 0x2) ? '1' : '0',
+    //        (state & 0x4) ? '1' : '0',
+    //        (state & 0x8) ? '1' : '0'
+    //     );
+    return state;
+}
+
+/**
+ * @brief Apply action a on current state S.
+ *
+ * @param qlearn Q-learning instance.
+ * @param a Action.
+ * @return state_t
+ */
+state_t qlearn_apply_action(qlearn_t *qlearn, action_t a)
+{
+    qlearn->apply_action(a);
+    return qlearn_get_state(qlearn);
+}
+
+/**
+ * @brief Get maximum Q-value at state S entry in Q-table.
+ *
+ * @param qlearn Q-learning instance.
+ * @param S State.
+ * @return qvalue_t
+ */
+qvalue_t qlearn_get_max_qvalue(qlearn_t *qlearn, state_t S){
+    qvalue_t Q_max = qlearn->table[S][0];
+
+    for (uint16_t i = 1; i < qlearn->n_actions; i++)
+    {
+        if (qlearn->table[S][i] > Q_max)
+        {
+            Q_max = qlearn->table[S][i];
+        }
+    }
+
+    return Q_max;
+}
+
+/**
+ * @brief Get action a associated with highest Q-value for state S.
+ *
+ * NB: Epsilon greedy is used to balance exploration and exploitation during
+ * training. ε starts from 1.0f and slowly decrease to 0.0f with more training
+ * episodes.
+ *
+ * @param qlearn Q-learning instance.
+ * @param S State.
+ * @return action_t
+ */
+action_t qlearn_get_action(qlearn_t *qlearn, state_t S){
+    action_t a;
+
+    // Choose random action with ε probability
+    if (rand() <= RAND_MAX * qlearn->epsilon)
+    {
+        a = rand() % qlearn->n_actions;
+    }
+    else
+    {
+        a = 0;
+        qvalue_t Q_max = qlearn->table[S][0];
+        for (uint16_t i = 1; i < qlearn->n_actions; i++)
+        {
+            if (qlearn->table[S][i] > Q_max)
+            {
+                Q_max = qlearn->table[S][i];
+                a = i;
+            }
+        }
+    }
+
+    return a;
+}
+
+/**
+ * @brief Update value Q(S, a) in Q-table using Bellman equation.
+ *
+ * @param qlearn Q-learning instance.
+ * @param S State.
+ * @param a Action.
+ * @param R Reward.
+ * @param Q_max Maximum future Q-value.
+ */
+void qlearn_update_qvalue(qlearn_t *qlearn, state_t S, action_t a, reward_t R, qvalue_t Q_max){
+    // Get old Q value for state S and action a
+    qvalue_t Q = qlearn->table[S][a];
+
+    // Compute Bellman equation
+    Q += (qlearn->learning_rate * (R + qlearn->discount_factor * Q_max - Q));
+
+    // Update Q value
+    qlearn->table[S][a] = Q;
+}
+
+void raylib_start(void){
+    
+    srand((unsigned int)time(NULL));
+    // Create game instance
+	game_init();
+
+	/* Q-learning initialization -------------------------------------------- */
+
+	qtable_conf_t qtable_conf = {.n_states = QLEARN_N_STATES,
+								 .n_actions = QLEARN_N_ACTIONS,
+								 .learning_rate = QLEARN_LEARNING_RATE,
+								 .discount_factor = QLEARN_DISCOUNT_FACTOR,
+								 .epsilon_rate = QLEARN_EPSILON_RATE};
+
+	qlearn_conf_t qlearn_conf = {.restart = game_restart,
+								 .is_ended = game_is_ended,
+								 .apply_action = game_apply_move,
+								 .get_state = game_get_state,
+								 .get_reward = game_get_reward,
+								 .params = &qtable_conf};
+
+	qlearn_t *qlearn = qlearn_init(&qlearn_conf);
+    //qlearn_load_table(qlearn, "qlearn_data.txt");
+    
+    bool bModelLoaded = false;
+    while (!WindowShouldClose()) 
+    {
+         //Train
+      
+        state_t S = qlearn_get_state(qlearn);
+
+        while (!qlearn_is_ended(qlearn))
+        {
+            // Load tarined model
+            if(IsKeyDown(KEY_SPACE) && !bModelLoaded)
+            {
+                qlearn_load_table(qlearn, "qlearn_data.txt");
+                bModelLoaded = true;
+            }
+
+            // get random or best action depending on epsilon
+            action_t a = qlearn_get_action(qlearn, S);
+
+            // get new game state after performing action
+            qvalue_t S_new = qlearn_apply_action(qlearn, a);
+
+            // learn only if e is not 0
+            if(qlearn->epsilon > 0.00000001f)
+            {
+                // Get reward from last performed action
+                reward_t reward = qlearn->get_reward();
+
+                // Get Q_Max from the new game state
+                qvalue_t Q_max = qlearn_get_max_qvalue(qlearn, S_new);
+
+                // Update QValue of the last game state
+                qlearn_update_qvalue(qlearn, S, a, reward, Q_max);
+            }
+            S = S_new;
+
+            /* Draw --------------------------------------------------------- */
+
+            game_draw();
+        }
+
+        qlearn_restart(qlearn);
+
+        // save here since the loop never exits
+        qlearn_save_table(qlearn, "qlearn_data.txt");
+        
+    }
+    
+    qlearn_deinit(qlearn);
+    game_deinit();
+}
---
+++ src/game.c
@@ -0,0 +1,379 @@
+#include "game.h"
+#include <raylib.h>
+#include "raymath.h"
+#include <float.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <limits.h>
+
+static size_t factor = 60;
+typedef struct ball_s {
+
+	float x, y; 
+	int w,h; 
+	int dx, dy;
+} ball_t;
+
+typedef struct paddle {
+    float x,y;
+	int w,h;
+} paddle_t;
+
+static ball_t ball;
+#define MAX_BALL_SPEED 150//60
+const int ball_max_speed = MAX_BALL_SPEED;
+const float paddle_speed = MAX_BALL_SPEED * 2.75f/*5*/;
+static paddle_t paddle[2];
+int score[] = {0,0};
+static float dist_ai_ball = {FLT_MAX};
+
+static Vector2 paddle_dims = {.x=10,.y=40};
+
+#define REWARD_CLOSE +10
+#define REWARD_FAR -10
+
+int check_collision(ball_t a, paddle_t b) {
+	if (a.x > b.x + b.w) {
+		return 0;
+	}
+	if (a.x + a.w < b.x) {
+		return 0;
+	}
+	if (a.y > b.y + b.h) {
+		return 0;
+	}
+	if (a.y + a.h < b.y) {
+		return 0;
+	}
+
+	return 1;
+}
+
+void move_ball(float dt) {
+	int w = GetScreenWidth();
+    int h = GetScreenHeight();
+
+	/* Move the ball by its motion vector. */
+	ball.x += ball.dx * ball_max_speed * dt;
+	ball.y += ball.dy * ball_max_speed * dt;
+	
+	/* Turn the ball around if it hits the edge of the screen. */
+	if (ball.x < 0) {
+		
+		score[1] += 1;
+		game_restart();
+	}
+
+	if (ball.x > w - ball.w) { 
+		
+		score[0] += 1;
+		game_restart();
+	}
+
+	if (ball.y < 0 || ball.y > h - ball.h) {
+		
+		ball.dy = -ball.dy;
+	}
+
+	//check for collision with the paddle
+	int i;
+
+	for (i = 0; i < 2; i++) {
+		
+		int c = check_collision(ball, paddle[i]); 
+
+		//collision detected	
+		if (c == 1) {
+			int max_Xspeed = 20;
+			//ball moving left
+			if (ball.dx < 0 && ball.dx > -max_Xspeed) {
+					
+				ball.dx -= 1;
+
+			//ball moving right
+			} else if(ball.dx < max_Xspeed){
+					
+				ball.dx += 1;
+			}
+			
+			//change ball direction
+			ball.dx = -ball.dx;
+			
+			//change ball angle based on where on the paddle it hit
+			int hit_pos = (paddle[i].y + paddle[i].h) - ball.y;
+
+			if (hit_pos >= 0 && hit_pos < 7) {
+				ball.dy = 4;
+			}
+
+			if (hit_pos >= 7 && hit_pos < 14) {
+				ball.dy = 3;
+			}
+			
+			if (hit_pos >= 14 && hit_pos < 21) {
+				ball.dy = 2;
+			}
+
+			if (hit_pos >= 21 && hit_pos < 28) {
+				ball.dy = 1;
+			}
+
+			if (hit_pos >= 28 && hit_pos < 32) {
+				ball.dy = 0;
+			}
+
+			if (hit_pos >= 32 && hit_pos < 39) {
+				ball.dy = -1;
+			}
+
+			if (hit_pos >= 39 && hit_pos < 46) {
+				ball.dy = -2;
+			}
+
+			if (hit_pos >= 46 && hit_pos < 53) {
+				ball.dy = -3;
+			}
+
+			if (hit_pos >= 53 && hit_pos <= 60) {
+				ball.dy = -4;
+			}
+
+            ball.dy += -1 + GetRandomValue(0,1) * 2; 
+
+			//ball moving right
+			if (ball.dx > 0) {
+
+				//teleport ball to avoid mutli collision glitch
+				if (ball.x < paddle_dims.x * 4) {
+				
+					ball.x = paddle_dims.x * 4;
+				}
+				
+			//ball moving left
+			} else {
+				
+				//teleport ball to avoid mutli collision glitch
+				if (ball.x > w - paddle_dims.x * 4) {
+				
+					ball.x = w - paddle_dims.x * 4;
+				}
+			}
+		}
+	}
+}
+
+void move_paddle(int dir,int paddle_id)
+{
+	paddle[paddle_id].y += dir * paddle_speed * GetFrameTime();
+    if(paddle[paddle_id].y > GetScreenHeight() - paddle[paddle_id].h)
+	{
+        paddle[paddle_id].y = GetScreenHeight() - paddle[paddle_id].h;
+    }
+    if(paddle[paddle_id].y < 0)
+	{
+        paddle[paddle_id].y = 0;
+    }
+}
+void move_paddle_ai(float dt, int paddle_id) 
+{
+	int center = paddle[paddle_id].y + paddle[paddle_id].h * 0.5f;
+	int screen_center = GetScreenHeight() - paddle[paddle_id].h * 0.5f;
+    SetRandomSeed(time(0));
+    int r_dir = GetRandomValue(1,4);
+    int dir = ball.dy > 0 ? r_dir  : -r_dir; 
+    if(dir < 0 && paddle[paddle_id].y < ball.y)
+	{
+        dir = r_dir;
+    }
+    if(dir > 0 && paddle[paddle_id].y > ball.y)
+	{
+        dir = -r_dir;
+    }
+	move_paddle(dir,paddle_id);
+}
+
+
+void game_init(void){
+    
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	SetTargetFPS(360);
+    InitWindow(factor*16, factor*9, "Pong");
+    game_restart();
+
+}
+void game_deinit(void){
+    CloseAudioDevice();
+    CloseWindow();
+}
+static size_t gridsize = 64; 
+static double lastTime = 0.0;
+#define NUM_GRID_SQUARES 8
+static int isPlayer = 0;
+
+void game_draw(void)
+{
+	// Update -------------------------------------------------------- //
+
+    float dt = GetTime() - lastTime;
+    lastTime = GetTime();
+
+	if(IsKeyReleased(KEY_P))
+	{
+		isPlayer = !isPlayer;
+	}
+	if(isPlayer)
+	{
+		if(IsKeyDown(KEY_UP))
+		{
+			move_paddle(-1,0);
+		}
+		if(IsKeyDown(KEY_DOWN))
+		{
+			move_paddle(1,0);
+		}
+	}
+	else 
+	{
+		move_paddle_ai(dt,0);
+	}
+    //move_paddle_ai(dt,1);
+    move_ball(dt);
+
+	// update distance between ball and ai paddle
+	Vector2 dist_vec = {ball.x - paddle[1].x, ball.y - paddle[1].y};
+	dist_ai_ball = sqrt(pow(dist_vec.x, 2) + pow(dist_vec.y, 2));
+
+	// Draw ---------------------------------------------------------- //
+
+    ClearBackground(GetColor(0x000000FF));
+    BeginDrawing();
+    int w = GetRenderWidth();
+    int h = GetRenderHeight();
+    for(int i = 0; i < 2;++i)
+	{
+        DrawRectangle(paddle[i].x,paddle[i].y,paddle[i].w,paddle[i].h,GetColor(0xFFFFFFFF));
+    }
+
+	// Draw lines
+	float ww = 5;
+	Rectangle rect = {w * 0.5f - ww * 0.5f,paddle_dims.y * 0.25f,ww,paddle_dims.y - paddle_dims.y * 0.3f};
+	for(int i = 0;rect.y < h;rect.y = i * (rect.height * 2))
+	{
+		DrawRectangleRec(rect,WHITE);
+		++i;
+	}
+
+    DrawRectangle(ball.x,ball.y,ball.w,ball.h,GetColor(0xFFFFFFFF));
+    char s_text[64] = {0};
+    snprintf(s_text,64,"%d",score[0]);
+    int fontSize = 24;
+    DrawText(s_text,w* 0.5f - 50,0,fontSize,GetColor(0xFF0000FF));
+	snprintf(s_text,64,"%d",score[1]);
+	DrawText(s_text,w* 0.5f + 50 - ww*2,0,fontSize,GetColor(0xFF0000FF));
+    
+    EndDrawing();
+}
+
+/**
+ * @brief Restart game.
+ *
+ */
+void game_restart(void){
+
+    int w = GetScreenWidth();
+    int h = GetScreenHeight();
+    ball.x = w * 0.5f - paddle_dims.x;
+    ball.y = h * 0.5f - paddle_dims.x;
+    ball.w = paddle_dims.x;
+	ball.h = paddle_dims.x;
+    ball.dx = -1 * 4;
+	ball.dy = -1 + GetRandomValue(0,1) * 4;
+	
+	paddle[0].x = 0;
+	paddle[0].y = h * 0.5f - paddle_dims.y;
+	paddle[0].w = paddle_dims.x;
+	paddle[0].h = paddle_dims.y;
+
+	paddle[1].x = w - paddle_dims.x;
+	paddle[1].y = h * 0.5f - paddle_dims.y;
+	paddle[1].w = paddle_dims.x;
+	paddle[1].h = paddle_dims.y;
+
+	// maybe add extra steps
+	dist_ai_ball = 0.f;
+}
+
+/**
+ * @brief Move paddle.
+ *
+ * @param move Player move: IDLE, UP, DOWN, LEFT, or RIGHT.
+ *
+ */
+void game_apply_move( uint8_t move)
+{
+    if(move == IDLE) return;
+	switch(move)
+	{
+		case UP:
+		move_paddle(-1, 1);
+		break;
+		case DOWN:
+		move_paddle(1, 1);
+		break;
+		default:
+		break;
+	}
+}
+
+/**
+ * @brief Check if the game ended (i.e., player on end).
+ *
+ * @return true
+ * @return false
+ */
+bool game_is_ended(void)
+{
+    if(score[0] == 50 || score[1] == 3)
+	{
+		score[0] = 0;
+		score[1] = 0;
+		return 1;
+	}
+    return 0;
+}
+
+/**
+ * @brief Get 8-bit state representation from game instance.
+ *
+ * @return uint16_t
+ */
+uint16_t game_get_state(void)
+{
+	paddle_t p = paddle[1];
+
+	uint8_t state = 0;
+	state |= (ball.y > p.y) << 0;
+	state |= (ball.y < p.y) << 1;
+	state |= (ball.x > p.x) << 2;
+	state |= (ball.x < p.x) << 3;
+	
+    return state;
+}
+
+/**
+ * @brief Get reward for making last move.
+ *
+ * @return int16_t
+ */
+int16_t game_get_reward(void)
+{
+	Vector2 dist_vec = {ball.x - paddle[1].x, ball.y - paddle[1].y};
+    float curr_dist = sqrt(pow(dist_vec.x, 2) + pow(dist_vec.y, 2));
+	if(curr_dist < dist_ai_ball)
+	{
+		return REWARD_CLOSE;
+	}	
+    return REWARD_FAR;
+}
